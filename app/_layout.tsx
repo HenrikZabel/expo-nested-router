6e0d2dba604bc46d208f03f812d7f00925c8a914
Henrik <54895728+HenrikZabel@users.noreply.github.com>
2024-11-06 22:01:32 +0100

    Workaround

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,5 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "Base screen" }} />
-      <Stack.Screen
-        name="nested1"
-        options={{ headerTitle: "Nested Screen 1", headerShown: false }}
-      />
-    </Stack>
-  );
+  return <Stack />;
 }
